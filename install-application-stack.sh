13a748d623ef936b273e57f4f66973d2654af087
Reza Ghaffari <rghaffari@gmail.com>
2023-06-27 00:59:29 -0400

    update airflow values and installation script

--- install-application-stack.sh
+++ install-application-stack.sh
@@ -82,7 +82,7 @@ echo -e "\n\n Installing Apache Airflow ..."
 cd /home/$INSTALLUSER/$RELEASEDIRNAME/airflow/
 helm repo add apache-airflow https://airflow.apache.org
 helm repo update
-helm install airflow apache-airflow/airflow -n airflow --create-namespace -f values.yaml
+helm install airflow ./chart/ -f ./values.yaml -n airflow --create-namespace
 kubectl exec airflow-worker-0 -n airflow -- airflow variables set POLYGON_API_KEY $POLYGON_API_KEY
 kubectl exec airflow-worker-0 -n airflow -- airflow variables set FINNHUB_API_KEY $FINNHUB_API_KEY
 
